c895fee4e3986aeb11fc97e3ff2bdadefcb9ab28
Van Hong <vanhong542002@gmail.com>
2024-12-17 11:50:45 +0700

    update

---
+++ src/pipelines/football_stadium_etl.py
@@ -0,0 +1,20 @@
+
+
+
+def extract():
+    """
+    Extracts data from the wikipedia page
+    """
+    pass
+
+def transform():
+    """
+    Transforms the data extracted from the wikipedia page
+    """
+    pass
+
+def load():
+    """
+    Loads the transformed data into a database
+    """
+    pass
